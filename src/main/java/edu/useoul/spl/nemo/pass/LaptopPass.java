7c0fa9e82c505c9bfdac2b936afbf7e36bbf7670
Jangho Seo <jangho@jangho.io>
2018-11-29 04:23:18 +0900

    fix answer

--- src/main/java/edu/useoul/spl/nemo/pass/LaptopPass.java
+++ src/main/java/edu/useoul/spl/nemo/pass/LaptopPass.java
@@ -3,13 +3,12 @@ package edu.useoul.spl.nemo.pass;
 import org.apache.nemo.common.dag.DAG;
 import org.apache.nemo.common.ir.edge.IREdge;
 import org.apache.nemo.common.ir.edge.executionproperty.CompressionProperty;
-import org.apache.nemo.common.ir.edge.executionproperty.DataFlowProperty;
 import org.apache.nemo.common.ir.edge.executionproperty.DataStoreProperty;
 import org.apache.nemo.common.ir.vertex.IRVertex;
 import org.apache.nemo.compiler.optimizer.pass.compiletime.annotating.Annotates;
 import org.apache.nemo.compiler.optimizer.pass.compiletime.annotating.AnnotatingPass;
 
-@Annotates({DataFlowProperty.class, DataStoreProperty.class})
+@Annotates({CompressionProperty.class, DataStoreProperty.class})
 public final class LaptopPass extends AnnotatingPass {
   public LaptopPass() {
     super(LaptopPass.class);
