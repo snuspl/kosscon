dabc4615102e9e042bcc72b8c919c4259ee87b1e
Jangho Seo <jangho@jangho.io>
2018-11-28 14:52:07 +0900

    Add Nemo

---
+++ src/main/java/edu/useoul/spl/nemo/App.java
@@ -0,0 +1,13 @@
+package edu.useoul.spl.nemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
